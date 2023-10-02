fb9b7b88771b249e045006ecd789f2643bd1e22e
CodingCarlos23 <32432836+CodingCarlos23@users.noreply.github.com>
2023-10-01 21:25:58 -0400

    Add files via upload

---
+++ model/InstructorList.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.util.TreeSet;
+
+public class InstructorList {//maybe make it singleton
+	 private static InstructorList instance = null;
+	    private TreeSet<Instructor> instructors;
+
+	    private InstructorList() {
+	        instructors = new TreeSet<>();
+	    }
+
+	    public static InstructorList getInstance() {
+	        if (instance == null) {
+	            instance = new InstructorList();
+	        }
+	        return instance;
+	    }
+
+	    public void addInstructor(Instructor instructor) {
+	        instructors.add(instructor);
+	    }
+	    
+	    public void printInstructors() {
+	        for (Instructor instructor : instructors) {
+	            System.out.println(instructor);
+	            instructor.printSchedule();
+	            System.out.println();
+	        }
+	    }
+	    
+	    public TreeSet<Instructor> getInstructors() {
+			return instructors;
+		}
+
+		public void setInstructors(TreeSet<Instructor> instructors) {
+			this.instructors = instructors;
+		}
+
+		public static void setInstance(InstructorList instance) {
+			InstructorList.instance = instance;
+		}
+
+		public int getSize() {
+	        return instructors.size();
+	    }
+}
